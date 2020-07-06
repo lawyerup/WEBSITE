556da809c149a2bba2ffc72d17b252cafefdd855
Brett Severin <brett7everin@gmail.com>
2020-07-06 01:45:43 -0500

    Marquee cleanup

--- src/Sections/Affiliates/Marquee/Marquee.jsx
+++ src/Sections/Affiliates/Marquee/Marquee.jsx
@@ -1,24 +1,37 @@
 import React, { Fragment } from "react";
+import styled, { keyframes } from "styled-components";
 
 import "./Marquee.css";
 
-function Marquee({title, items}) {
-  const slides = items.map((i) => (
+function Marquee({ title, items }) {
+  var slides = items.map((i) => (
     <Fragment>
       <div class="slide">
-        <img src={i.image} height="100" width="250" alt={i.name} />
+        <img src={i.image} alt={i.name} />
       </div>
     </Fragment>
   ));
 
+  const scroll = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(-10vw * ${slides.length}));
+  }
+  `;
+
+  const Scroll = styled.div`
+    animation: ${scroll} ${slides.length >= 7 ? 20 : 0}s linear infinite;
+    width: calc(-10vw * ${slides.length * 2});
+    ${slides.length <= 7 ? `justify-content: center;` : ""}
+  `;
+
   return (
     <Fragment>
-      <div className="marquee">
+      <div className={"marquee"}>
         <h1>{title}</h1>
-        <div className="slide-track">
-          {slides}
-          {slides}
-        </div>
+        <Scroll className="slide-track">{slides}{slides}</Scroll>
       </div>
     </Fragment>
   );
---
+++ src/Sections/Affiliates/data.tsx
@@ -0,0 +1,77 @@
+export const Partners = [
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png",
+  },
+];
+
+export const Universities = [
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  },
+  {
+    name: "name",
+    image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  },
+];
