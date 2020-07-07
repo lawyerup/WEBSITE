244b900be295f163d3ecb366b2b69ac8ed44a6b2
Brett Severin <brett7everin@gmail.com>
2020-07-07 01:31:33 -0500

    Continents split

--- src/Sections/Team/map.jsx
+++ src/Sections/Team/map.jsx
@@ -1,14 +1,44 @@
 import React, { Fragment } from "react";
 
-import { locations } from "./map_locations";
+import { northAmerica, southAmerica, europe, asia, africa, australia, locations } from "./map_locations";
 
 function Map() {
   const dot =
     "m 0,0 c 0,-2.945 -2.388,-5.333 -5.333,-5.333 -2.945,0 -5.333,2.388 -5.333,5.333 0,2.945 2.388,5.333 5.333,5.333 C -2.388,5.333 0,2.945 0,0";
 
-  const map = locations.map((d) => (
+  const northAmericaMap = northAmerica.map((d) => (
     <g transform={"translate(" + d.x + "," + d.y + ")"}>
-      <path d={dot} fill="white" />
+      <path d={dot} fill="red" />
+    </g>
+  ));
+
+  const southAmericaMap = southAmerica.map((d) => (
+    <g transform={"translate(" + d.x + "," + d.y + ")"}>
+      <path d={dot} fill="green" />
+    </g>
+  ));
+
+  const europeMap = europe.map((d) => (
+    <g transform={"translate(" + d.x + "," + d.y + ")"}>
+      <path d={dot} fill="dodgerBlue" />
+    </g>
+  ));
+
+  const asiaMap = asia.map((d) => (
+    <g transform={"translate(" + d.x + "," + d.y + ")"}>
+      <path d={dot} fill="orange" />
+    </g>
+  ));
+
+  const africaMap = africa.map((d) => (
+    <g transform={"translate(" + d.x + "," + d.y + ")"}>
+      <path d={dot} fill="brown" />
+    </g>
+  ));
+
+  const australiaMap = australia.map((d) => (
+    <g transform={"translate(" + d.x + "," + d.y + ")"}>
+      <path d={dot} fill="purple" />
     </g>
   ));
 
@@ -22,7 +52,14 @@ function Map() {
     >
       <g transform="matrix(1.25,0,0,-1.25,0,1312.5)">
         <g>
-          <g clip-path="url(#clipPath16)">{map}</g>
+          <g clip-path="url(#clipPath16)">
+            {northAmericaMap}
+            {southAmericaMap}
+            {europeMap}
+            {asiaMap}
+            {africaMap}
+            {australiaMap}
+          </g>
         </g>
       </g>
     </svg>
