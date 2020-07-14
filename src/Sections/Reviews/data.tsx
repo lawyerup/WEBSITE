c2b3d8e1d549ffcea7dde85d7351a975029bb307
Brett Severin <brett7everin@gmail.com>
2020-07-14 02:57:39 -0500

    Update data.tsx

--- src/Sections/Reviews/data.tsx
+++ src/Sections/Reviews/data.tsx
@@ -28,18 +28,10 @@ export const UserReviews: Review[] = [
   },
   {
     text:
-      "ScholarUp has been an extremely unique learning experience for me. Not only has it encouraged me to explore more into questions of law that I hadn't encountered before, but it has also given me the opportunity to share my thoughts with students from different countries and cultures which is not something I've gotten the chance to do before. I want to sincerely thank the extremely dedicated group of students that make ScholarUp possible for helping me broaden my perspective.",
-    name: "Anushka Gupta",
-    year: "20",
-    id: "2",
-    image: anushka_gupta,
-  },
-  {
-    text:
       "The LawyerUp program has turned what I thought we would be a hopeless time into a hopeful one. The CoVID-19 pandemic didn't stop us. I am so grateful to the Lawyerup team because this program has opened up my mind to the different sectors of law. It's given me more networking opportunities than I could've ever imagined.",
     name: "Cassidi Mignoulo",
     year: "20",
-    id: "3",
+    id: "2",
     image: cassidi_mignuolo,
   },
   {
@@ -47,48 +39,48 @@ export const UserReviews: Review[] = [
       "LawyerUp offers an 'inner internship' called ScholarUp which I am so thankful for as it has greatly enhanced my thinking, research and writing in the law, and has helped me think more creatively when it comes to law! It has really tested my values and morals, and has required me to deeply reflect on the weekly topics given such as pillars of punishment, and mental health and law. ScholarUp is a challenge and I am up for it!",
     name: "Cynthia Vitolo",
     year: "20",
-    id: "4",
+    id: "3",
     image: cynthia_vitolo},
   {
     text:
       "Being able to hear from so many motivated lawyers about the realities of law alongside so many future lawyers has given me this new drive to better myself in all aspects. LawyerUp has given me a new burst of knowledge and I didn't even have to leave my home to receive that!",
     name: "Galila Assefa",
     year: "20",
-    id: "5",
+    id: "4",
     image: galila_assefa},
   {
     text:
       "I’m so lucky to be a part of the LawyerUp internship program! I had the fantastic opportunity to learn about the legal field in real life and interact with layers from multiple areas of law to find out what we are interested in. ScholarUp is another great platform for students to further their understanding in the legal system while sharpening professional skills such as communication and teamwork. Join LawyerUp and I promise you won’t regret it!",
     name: "Linjing Yu",
     year: "20",
-    id: "6",
+    id: "5",
     image: linjing_yu},
   {
     text:
       "LawyerUp is an amazing program that provides aspiring lawyers with the knowledge they need from working lawyers and professionals in the field. We are able to learn from a diverse group of people that have gone through many struggles and achieved great things throughout their career. It provides us with the unique insight into what being a lawyer in today's world is like. ",
     name: "Daisy Garcia",
     year: "20",
-    id: "7",
+    id: "6",
     image: daisy_garcia},
   {
     text:
       "LawyerUp and ScholarUP have given me the opportunity to interact with like minded individuals and attorneys on a personal level. I have received tips on how to write better, connect with attorneys, and improve my writing. This internship has provided me with more than I could have ever imagined!",
     name: "Zaquariya Patterson",
     year: "20",
-    id: "8",
+    id: "7",
     image: zaquariya_patterson},
   {
     text:
       "Coming across LawyerUp was the best thing that has happened to me during this pandemic. The exposure and insight I have received from each speaker during our zoom calls has really helped inform my decision with law school. Whether I go to law school or not, the people I’ve met and the knowledge everyone has shared will stay with me forever!",
     name: "Hesham Akhoon",
     year: "20",
-    id: "9",
+    id: "8",
     image: hesham_akhoon},
   {
     text:
-      "At the start of the CoVID-19 lockdown, I felt lost. It seemed as if I had no purpose. However, this internship gave me something to look forward to. Furthermore, the fact that it is a virtual internship made it accessible for anyone around the globe, including myself in the UK. LawyerUp gave me the opportunity to learn about several different areas of law - both broad and niche areas. I also created a virtual network with like-minded people as well as a platform to share new opportunities. I have learnt so much from the programme already and I look forward to what is still to come.",
+      "At the start of the CoVID-19 lockdown, I felt lost. It seemed as if I had no purpose. However, this internship gave me something to look forward to. Furthermore, the fact that it is a virtual internship made it accessible for anyone around the globe, including myself in the UK. LawyerUp gave me the opportunity to learn about several different areas of law - both broad and niche areas.",
     name: "Karishma Khosla",
     year: "20",
-    id: "10",
+    id: "9",
     image: karishma_khosla},
 ];
