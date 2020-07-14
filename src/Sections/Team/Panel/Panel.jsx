4ed38a32f04d8c99ab6c109c855afc58da091d54
Brett Severin <brett7everin@gmail.com>
2020-07-13 21:00:15 -0500

    Universities

--- src/Sections/Team/Panel/Panel.jsx
+++ src/Sections/Team/Panel/Panel.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import "./Panel.css";
+import icon_linkedin from "../../../Images/icons/icon_linkedin.svg";
 
 function Panel({ continent, directors }) {
   const id = continent.toLowerCase().replace(/\s/g, "") + "Panel";
@@ -8,7 +9,9 @@ function Panel({ continent, directors }) {
       <div className="director">
         <img src={d.image} alt="test" />
         <div className="content">
-          <h1>{d.name}</h1>
+          <h1>
+            {d.name} <a hidden={d.link ? false : true} target={"_blank"} href={d.link}><img src={icon_linkedin} alt={"linkedIn"} /></a>
+          </h1>
           <label>{d.bio}</label>
         </div>
       </div>
@@ -20,9 +23,7 @@ function Panel({ continent, directors }) {
       <div id={id} className={"panel"}>
         <h1>
           {continent} Director{directors.length > 1 ? "s" : ""}
-          <button className={"material-icons"}>
-            close
-          </button>
+          <button className={"material-icons"}>close</button>
         </h1>
         {directorList}
       </div>
